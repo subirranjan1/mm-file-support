83f5473007e3da85cab0fae49598b22d03eb2581
Michael Nixon <mnixon@gmail.com>
2014-11-11 13:44:58 -0800

    new logo, new open in nova

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,7 @@ FileSupportMovingstories::Application.configure do
   # }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_options = {from: 'no-reply@sfu.ca'}
+  config.action_mailer.default_options = {from: 'no-reply@movingstories.ca'}
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
--- config/routes.rb
+++ config/routes.rb
@@ -1,36 +1,33 @@
 FileSupportMovingstories::Application.routes.draw do
-  get "password_resets/new"
-  apipie
   root 'projects#index'
+  get '/404' => 'errors#not_found'
+  get '/500' => 'errors#internal_error'
+  get '/422' => 'errors#unprocessable_entity'  
+  apipie
+  get "password_resets/new"
   get "pages/about"
   get "pages/contact"
-  resources :sensor_types
-  resources :movers
-  resources :access_groups
-  resources :movement_annotations
   get '/data_tracks/provide', to: 'data_tracks#provide'
   post '/data_tracks/import', to: 'data_tracks#import'
-  resources :data_tracks
-  resources :movement_groups
-  resources :projects
   get "myprojects" => "projects#mine", :as => "myprojects"
   get "log_out" => "sessions#destroy", :as => "log_out"
   get "log_in" => "sessions#new", :as => "log_in"
   get "sign_up" => "users#new", :as => "sign_up"
-#  root :to => "users#new"
+  resources :sensor_types
+  resources :movers
+  resources :access_groups
+  resources :movement_annotations
+  resources :data_tracks
+  resources :movement_groups
+  resources :projects
   resources :users, except: [:index]
   resources :sessions, only: [:new, :create, :destroy]
   resources :password_resets, except: [:index]
+  match 'tagged' => 'movement_groups#tagged', :via => [:get], :as => 'tagged'
+ 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
-  match 'tagged' => 'movement_groups#tagged', :via => [:get], :as => 'tagged'
- 
-  get '/404' => 'errors#not_found'
-  get '/500' => 'errors#internal_error'
-  get '/422' => 'errors#unprocessable_entity'
-
 end
