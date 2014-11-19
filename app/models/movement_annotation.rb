9a52941eae3648d163eb7c95d5c3159a1334568c
Michael Nixon <mnixon@gmail.com>
2014-11-18 23:30:06 -0800

    integrate annotations with projects, takes, tracks

--- app/models/movement_annotation.rb
+++ app/models/movement_annotation.rb
@@ -1,6 +1,6 @@
 class MovementAnnotation < ActiveRecord::Base
   has_one :asset, as: :attachable
-  belongs_to :annotated, polymorphic: true
+  belongs_to :attached, polymorphic: true
   belongs_to :owner, class_name: "User", foreign_key: "user_id"
   acts_as_taggable # Alias for acts_as_taggable_on :tags
   validates :name, presence: true
