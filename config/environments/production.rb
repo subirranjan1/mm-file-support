77d3b21fa4b1bb113cd4e12067ac74ee857c3eab
Michael Nixon <mnixon@gmail.com>
2014-11-19 20:27:09 -0800

    update type

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,4 +87,7 @@ FileSupportMovingstories::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
   config.action_mailer.default_url_options = { :host => "http://mnm-db.movingstories.ca" }
+  Paperclip.options[:content_type_mappings] = {
+    :c3d => "application/octet-stream"
+  }  
 end
