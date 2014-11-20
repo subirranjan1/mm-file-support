cdce535d9ef55c2dd02cbf499936c07400cfc312
Michael Nixon <mnixon@gmail.com>
2014-11-19 20:12:42 -0800

    fix association name

--- app/models/movement_group.rb
+++ app/models/movement_group.rb
@@ -2,7 +2,7 @@ class MovementGroup < ActiveRecord::Base
   belongs_to :project
   has_many :data_tracks, dependent: :destroy
   has_and_belongs_to_many :movers
-  has_and_belongs_to_many :sensors  
+  has_and_belongs_to_many :sensor_types  
   has_many :movement_annotations, as: :attached  
   acts_as_taggable # Alias for acts_as_taggable_on :tags
   belongs_to :owner, class_name: "User", foreign_key: "user_id"
