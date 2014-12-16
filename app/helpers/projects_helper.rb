a032fe8dcfb63bfbcf085f6d494a6216ca38a85f
Michael Nixon <mnixon@gmail.com>
2014-12-16 12:19:40 -0800

    update permissions access within groups etc

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -11,6 +11,14 @@ module ProjectsHelper
     current_most
   end
   
+  def group_link_or_name(take)
+    if take.is_accessible_by?(@current_user) or take.public?
+      link_to(truncate(take.name, length: 20), movement_group_path(take)) 
+    else
+      truncate(take.name, length: 20)
+    end
+  end
+  
   def mova_url
     #"http://www.sfu.ca/~oalemi/movan/"
     #"http://142.58.181.193:8080/index.php"
