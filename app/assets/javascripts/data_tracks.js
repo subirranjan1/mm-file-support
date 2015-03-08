015c7ff95f248c8341d83e4a3daab60c40ded058
Michael Nixon <mnixon@gmail.com>
2015-03-08 15:21:54 -0700

    switch to server side via normal javascript

---
+++ app/assets/javascripts/data_tracks.js
@@ -0,0 +1,12 @@
+jQuery(document).ready(function() {
+  $('#tracks-table').dataTable({
+    "processing": true,
+    "serverSide": true,
+    "ajax": $('#tracks-table').data('source'),
+    "pagingType": "full_numbers",
+    "columnDefs": [{
+        "bSortable": false,
+        "aTargets": ['nosort']
+    		}]
+  });
+});
--- app/assets/javascripts/data_tracks.js.coffee
+++
@@ -1,9 +0,0 @@
-$ ->
-  $('#tracks-table').dataTable
-    ajax: $('#tracks-table').data('source')
-		"pagingType": "full_numbers"
-		iTotalDisplayRecords: data.Count()
-    columnDefs: [{
-        "bSortable": false,
-        "aTargets": ['nosort']
-    		}]
