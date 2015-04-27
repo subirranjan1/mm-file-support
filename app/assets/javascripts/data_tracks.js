3cc7a52bebe310db541bfb9ec4da574c765f71da
Michael Nixon <mnixon@gmail.com>
2015-04-27 12:41:00 -0700

    try some different config

--- app/assets/javascripts/data_tracks.js
+++ app/assets/javascripts/data_tracks.js
@@ -2,6 +2,8 @@ jQuery(document).ready(function() {
   $('#tracks-table').dataTable({
     "processing": true,
     "serverSide": true,
+		"bDeferRender": true,
+		"bSortClasses": false,
     "ajax": $('#tracks-table').data('source'),
     "pagingType": "full_numbers",
     "columnDefs": [{
